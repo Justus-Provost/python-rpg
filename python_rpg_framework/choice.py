5310038df13c6042ec00e3562ec4234e9e57bebe
Justus-Provost <provj709@hsd.k12.or.us>
2023-05-02 14:58:43 -0700

    added json file and reader

--- python_rpg_framework/choice.py
+++ python_rpg_framework/choice.py
@@ -3,6 +3,23 @@ a set of functions to be used tto make the program more useful and be used
 in other similar projects."""
 
 # import statements
+from pathlib import Path
+
+def get_file_contents(path: str, filename: str) -> str:
+    """Returns the contents of the file in the path.
+    
+    Args:
+        path: the relative folder path with file name and extentions.(should end with forward slash)
+        filename: the name of the file with extention
+        
+    Returns:
+        contents: a string of the file contents."""
+    
+    folder = Path(path)
+    file_to_open = folder / filename
+    f = open(file_to_open)
+    return f.read()
+
 
 def get_choice(menu: str, legal_choices: tuple) -> str:
     """displays a menu of options, and asks the user to make a choice.
@@ -27,6 +44,8 @@ def get_choice(menu: str, legal_choices: tuple) -> str:
     return user_choice
 
 if __name__ == "__main__":
+    contents = get_file_contents("data/", "player.json")
+    print(contents)
     menu = "Here is your list of options:\n\t1 - Option #1\n"
     menu+= "\t2 - Option #2\n\t3 - Option #3\n\n"
     selection = get_choice(menu,("1","2","3"))
---
+++ python_rpg_framework/utilities.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+def get_file_contents(path: str, filename: str) -> str:
+    """Returns the contents of the file in the path.
+    
+    Args:
+        path: the relative folder path with file name and extentions.(should end with forward slash)
+        filename: the name of the file with extention
+        
+    Returns:
+        contents: a string of the file contents."""
+    
+    folder = Path(path)
+    file_to_open = folder / filename
+    f = open(file_to_open)
+    return f.read()
+
+
+
+if __name__ == "__main__":
+    contents = get_file_contents("data/", "player.json")
+    print(contents)
